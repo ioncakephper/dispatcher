d719a814244bdad41aaf9c910cdee990e2a96d8a
Ion Gireada <shytiger@yahoo.com>
2023-01-06 15:00:13 +0200

    Update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { camelCase, snakeCase, pascalCase, dotCase } = require("change-case");
 
 /**
- * A class that handles the dispatching of events.       
+ * A class that handles the dispatching of events.
  */
 class Dispatcher {
 
@@ -15,13 +15,47 @@ class Dispatcher {
             defaultPrefix: "default",
         }
     }
-    
+
     /**
-     * Dispatch a method call to the correct method.       
-     * @param {object} caller - The object that is calling the method.       
-     * @param {string} hook - The name of the method to call.       
-     * @param {any[]} parameters - The parameters to pass to the method.       
-     * @returns The return value of the method.       
+     * Dispatch a method call to the correct method.
+     * @param {object} caller - The object that is calling the method.
+     * @param {string} hook - The name of the method to call.
+     * @param {any[]} parameters - The parameters to pass to the method.
+     * @returns The return value of the method.
+     * @example
+     *
+     * class MyClass {
+     *  // you will invoke find with a hook name as first parameter.
+     *  // Remaining parameters will be passed to hook method.
+     *  find(hook) {
+     *      let d = new Dispatcher();
+     *      return d.dispatch('find', arguments)
+     *  }
+     *
+     *
+      * // Find all the sites that match the given options.
+      * // @param {object} options - the options to find sites with.
+      * // @returns {object[]} - an array of objects containing the site properties.
+     *  findAll(options = {}) {
+     *      return [{}]
+     *  }
+     *
+     *  findRange(offset, count, options = {}) {
+     *     options = {
+     *         ...options,
+     *         ...{offset, count}
+     *     }
+     *     return this.find('all', options)
+     *  }
+     * }
+     *
+     * // In your code:
+     *
+     * let m = new MyClass();
+     * let records = m.find('all', {}) // will dispatch find's all hook to findAll method.
+     *
+     * // or,if the caller is a string, it's a hook name, and the parameters are the hook parameters.
+     *  let records = m.find('range', 10, 25, options) // will dispatch find's range hook to findRange method, which invoke find's all hook
      */
     dispatch(caller, hook, parameters) {
         let args = [];
@@ -43,11 +77,11 @@ class Dispatcher {
     }
 
     /**
-     * Builds a method name for the given base and hook.       
-     * @param {string} base - the base of the method name.       
-     * @param {string} hook - the hook of the method name.       
-     * @param {object} [options={}] - the options for the method name.       
-     * @returns {string} the method name.       
+     * Builds a method name for the given base and hook.
+     * @param {string} base - the base of the method name.
+     * @param {string} hook - the hook of the method name.
+     * @param {object} [options={}] - the options for the method name.
+     * @returns {string} the method name.
      */
     buildMethodName(base, hook, options = {}) {
         options = {
@@ -68,9 +102,9 @@ class Dispatcher {
     }
 
     /**
-     * Builds the default method name for the given hook.       
-     * @param {string} hook - the hook to build the method name for.       
-     * @returns {string} the default method name for the given hook.       
+     * Builds the default method name for the given hook.
+     * @param {string} hook - the hook to build the method name for.
+     * @returns {string} the default method name for the given hook.
      */
     buildDefaultMethodName(hook) {
         options = {
@@ -81,10 +115,10 @@ class Dispatcher {
     }
 
     /**
-     * Checks if a method exists in a given object.       
-     * @param {object} caller - the object to check for the method in       
-     * @param {string} fn - the name of the method to check for       
-     * @returns {boolean} - true if the method exists, false otherwise       
+     * Checks if a method exists in a given object.
+     * @param {object} caller - the object to check for the method in
+     * @param {string} fn - the name of the method to check for
+     * @returns {boolean} - true if the method exists, false otherwise
      */
     methodExists(caller, fn) {
         let e = `typeof caller['${fn}'] === 'function'`
@@ -92,11 +126,11 @@ class Dispatcher {
     }
 
     /**
-     * Calls a method on the caller object.           
-     * @param {object} caller - the object that contains the method.           
-     * @param {string} fn - the name of the method to call.           
-     * @param {any[]} params - the parameters to pass to the method.           
-     * @returns the result of the method call.           
+     * Calls a method on the caller object.
+     * @param {object} caller - the object that contains the method.
+     * @param {string} fn - the name of the method to call.
+     * @param {any[]} params - the parameters to pass to the method.
+     * @returns the result of the method call.
      */
     callMethod(caller, fn, params) {
         let args = params.map(p => {
